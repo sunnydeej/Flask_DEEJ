b8b7a58aca191d0824d3d6f8086183bbb3ef1b52
Scott Lewis <scottydro@gmail.com>
2023-03-02 00:08:20 -0500

    MAJ UPDATE

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-    
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
